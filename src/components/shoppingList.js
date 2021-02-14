6cf8ae3c0faac3db256790e340d41b3f5f710591
Fizioh <batrom@laposte.net>
2021-02-14 14:12:31 +0100

    composant shoppinglist

---
+++ src/components/shoppingList.js
@@ -0,0 +1,19 @@
+const plantList = [
+    'monstera',
+    'ficus lyrata',
+    'pothos argenté',
+    'yucca',
+    'palmier'
+]
+
+function ShoppingList() {
+    return(
+        <div>
+            {plantList.map((plant) =>(
+                <li>{plant}</li>
+            ))}
+        </div>
+    )
+}
+
+export default ShoppingList
