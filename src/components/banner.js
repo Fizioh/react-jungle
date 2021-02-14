a7fc950d6db2043ce0752b63ed98e4eca42c2cd5
Fizioh <batrom@laposte.net>
2021-02-14 11:18:09 +0100

    import logo

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,10 +1,12 @@
-import '../styles/banner.css'
+import '../styles/banner.css';
+import logo from '../assets/logo.png';
 
 function Banner() {
     const titre = "La maison jungle";
 
 
     return <div className="lmj-banner">
+        <img src={logo} alt="la maison jungle" className="lmj-logo" />
         <h1>{ titre }</h1>
         </div>
 }
