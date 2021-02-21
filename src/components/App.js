49ed9955d3c29ed793e69f68dfc775e88ea204ee
Fizioh <batrom@laposte.net>
2021-02-21 19:25:56 +0100

    change BestSale

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import Banner from './banner';
-import Cart from './cart';
-import ShoppingList from'./shoppingList';
+
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+// import Cart from './Cart'
+import ShoppingList from './ShoppingList'
 
 function App() {
-  return <div> 
-    <Banner />
-  <Cart />
-  <ShoppingList />
-  </div>
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
 }
 
-export default App;
+export default App
