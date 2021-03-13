784ad2b8a5bbddd1d5961dec26050ffa983441d1
Fizioh <batrom@laposte.net>
2021-03-13 14:00:05 +0100

    block value

--- src/components/questionForm.js
+++ src/components/questionForm.js
@@ -4,20 +4,20 @@ import { useState } from 'react';
 
 function QuestionForm(){
     const [inputValue, setInputValue] = useState('Posez votre question ici')
-    const isInputError = inputValue.includes('f')
+    function checkValue(value){
+        if(!value.includes('f')){
+            setInputValue(value)
+        }
+    }
 
     
     return(
         <div>
-            
-
             <textarea 
             value={inputValue}
-            onChange= {(e) => setInputValue(e.target.value)}
+            onChange={(e) => checkValue(e.target.value)}
             />
-            {isInputError && (
-            <div>Vous n'avez pas le droit d'utiliser la lettre "f" ici.</div>
-            )}
+            
             <button onClick={() => alert(inputValue)}>Alertez moi !</button>
 
             
