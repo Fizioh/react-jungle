50c6e147abd47d700c80eefc34bef20b6abd555d
Fizioh <batrom@laposte.net>
2021-03-13 13:56:12 +0100

    controlled form

--- src/components/questionForm.js
+++ src/components/questionForm.js
@@ -4,13 +4,23 @@ import { useState } from 'react';
 
 function QuestionForm(){
     const [inputValue, setInputValue] = useState('Posez votre question ici')
+    const isInputError = inputValue.includes('f')
+
+    
     return(
         <div>
+            
+
             <textarea 
             value={inputValue}
             onChange= {(e) => setInputValue(e.target.value)}
             />
+            {isInputError && (
+            <div>Vous n'avez pas le droit d'utiliser la lettre "f" ici.</div>
+            )}
             <button onClick={() => alert(inputValue)}>Alertez moi !</button>
+
+            
         </div>
     )
 
