9f7bd2451279d9ea43f28238802cd97b85b8aa2f
Fizioh <batrom@laposte.net>
2021-03-13 13:39:21 +0100

    useState form

--- src/components/questionForm.js
+++ src/components/questionForm.js
@@ -1,17 +1,18 @@
 
+import { useState } from 'react';
+
 
 function QuestionForm(){
-
-    function handleSubmit(e){
-        e.preventDefault()
-        alert(e.target['my_input'].value)
-    }
+    const [inputValue, setInputValue] = useState('Posez votre question ici')
     return(
-    <form onSubmit={handleSubmit}>
-        <input type='text' name ='my_input' defaultValue='Tapez votre texte' />
-        <button type='submit'>Entrer</button>
-        </form>
-        )
+        <div>
+            <textarea 
+            value={inputValue}
+            onChange= {(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={() => alert(inputValue)}>Alertez moi !</button>
+        </div>
+    )
 
 }
 
