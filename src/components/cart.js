1277992f8ff35a6c804445259b7799a1410210cd
Fizioh <batrom@laposte.net>
2021-03-15 19:10:11 +0100

    cart destructuration

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,10 +1,9 @@
 import '../styles/cart.css';
 import { useState } from 'react'
 
-function Cart() {
+function Cart({cart, updateCart}) {
     const monsteraPrice = 8
-    const [cart, updateCart] = useState(0)
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(true)
 
     return isOpen ? (
 
@@ -14,19 +13,12 @@ function Cart() {
 				      onClick={() => setIsOpen(false)}
 			        >
 				      Fermer
+
 			        </button>
 
                   <h2>Panier</h2>
-                   <div>
-                   Monstera : {monsteraPrice}$
-                  <button onClick={() => updateCart(cart + 1)}>
-                   Ajouter
-                  </button>
-                  <button onClick={() => updateCart(0)}>
-                   Vider
-                  </button>
-                  </div>
                   <h3>Total : {monsteraPrice * cart}$</h3>
+                  
         </div>
       ) : (
             <div className='lmj-cart-closed'>
