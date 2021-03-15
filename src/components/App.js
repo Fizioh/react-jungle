2aa1f34fe7fab3fb45361637261b0f720bc23a2e
Fizioh <batrom@laposte.net>
2021-03-15 19:02:40 +0100

    useState cart in app

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,18 @@ import ShoppingList from './shoppingList'
 import Footer from './footer'
 
 function App() {
+	const [cart, updateCart] = useState([])
+
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-			<Cart />
-			<ShoppingList />
+			<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
 			<Footer />
 		</div>
 	)
